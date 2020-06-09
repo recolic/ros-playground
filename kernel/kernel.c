a3e9b20814de2b9db7e84b7e3bd98679f9bc504f
Recolic Keghart <root@recolic.net>
2020-06-09 17:11:37 +0800

    naive test kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,20 @@
 
+
+char *vga_begin = (char *)0xb8000;
+void push_char(char c, char color) {
+    static int pos = 0;
+    vga_begin[pos++] = c;
+    vga_begin[pos++] = color;
+}
+
 void main() {
-    char *vga_begin = (char *)0xb8000;
-    *vga_begin = 'X';
+    int pos = 0;
+    for(int cter = 0; cter < 256; ++cter) {
+        push_char(cter % 10 + '0', cter);
+        for(int i = 1; i < 10; ++i) {
+            push_char(i + '0', cter);
+        }
+    }
+
 }
 
